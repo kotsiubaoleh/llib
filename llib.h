11552986ad3e7b8ee219a251cc74cb6e35be5f47
kotsiubaoleh <kotsiubaoleh@gmail.com>
2018-11-15 22:58:28 +0200

    Added RB Tree insert fixup macro

--- llib.h
+++ llib.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 
+#define _COLOR_RED 0
+#define _COLOR_BLACK 1
+
 typedef struct llib_rb_node_t {
 	struct llib_rb_node_t *parent;
 	struct llib_rb_node_t *left;
@@ -11,17 +14,66 @@ typedef struct llib_rb_node_t {
 	char color;
 } llib_rb_node_t;
 
-llib_rb_node_t _rb_null = {0, 0, 0, 0, 1};
+llib_rb_node_t _rb_null = {0, 0, 0, 0, _COLOR_BLACK};
 
 typedef llib_rb_node_t llib_rb_tree_t;
 
 llib_rb_node_t *_node_arr[64] = {0};
 
-#define LLIB_INIT_RB_TREE(tree) (*tree = &_rb_null)
+#define _LLIB_RB_IS_RIGHT_CHILD(node) (node->parent->right == node)
 
-#define LLIB_RB_INSERT(tree, new_value_ptr, compare) {\
-	llib_rb_node_t *current_node = *tree;\
-	llib_rb_node_t *parent_node = 0;\
+#define LLIB_INIT_RB_TREE(root) (root = &_rb_null)
+
+#define _LLIB_RB_TRANSPLANT(old_node, node) {\
+	if (_LLIB_RB_IS_RIGHT_CHILD(old_node)) {\
+		old_node->parent->right = node;\
+	} else {\
+		old_node->parent->left = node;\
+	}\
+	node->parent = old_node->parent;\
+}
+
+#define _LLIB_RB_ROTATE_LEFT(root, node) {\
+	llib_rb_node_t *_node_to_rotate = node;\
+	llib_rb_node_t *_right_child = _node_to_rotate->right;\
+	_right_child->parent = _node_to_rotate->parent;\
+	if (_right_child->parent == &_rb_null) {\
+		root = _right_child;\
+	} else if (_LLIB_RB_IS_RIGHT_CHILD(_node_to_rotate)) {\
+		_right_child->parent->right = _right_child;\
+	} else {\
+		_right_child->parent->left = _right_child;\
+	}\
+	_node_to_rotate->right = _right_child->left;\
+	if (_node_to_rotate->right != &_rb_null) {\
+		_node_to_rotate->right->parent = _node_to_rotate;\
+	}\
+	_right_child->left = _node_to_rotate;\
+	_node_to_rotate->parent = _right_child;\
+}
+
+#define _LLIB_RB_ROTATE_RIGHT(root, node) {\
+	llib_rb_node_t *_node_to_rotate = node;\
+	llib_rb_node_t *_left_child = _node_to_rotate->left;\
+	_left_child->parent = _node_to_rotate->parent;\
+	if (_left_child->parent == &_rb_null) {\
+		root = _left_child;\
+	} if (_LLIB_RB_IS_RIGHT_CHILD(_node_to_rotate)) {\
+		_left_child->parent->right = _left_child;\
+	} else {\
+		_left_child->parent->left = _left_child;\
+	}\
+	_node_to_rotate->left = _left_child->right;\
+	if (_node_to_rotate->left != &_rb_null) {\
+		_node_to_rotate->left->parent = _node_to_rotate;\
+	}\
+	_left_child->right = _node_to_rotate;\
+	_node_to_rotate->parent = _left_child;\
+}
+
+#define LLIB_RB_INSERT(root, new_value_ptr, compare) {\
+	llib_rb_node_t *current_node = root;\
+	llib_rb_node_t *parent_node = &_rb_null;\
 	char direction;\
 	while(current_node != &_rb_null) {\
 		parent_node = current_node;\
@@ -34,40 +86,84 @@ llib_rb_node_t *_node_arr[64] = {0};
 		}\
 	}\
 	current_node = malloc(sizeof(llib_rb_node_t));\
-	if (parent_node != 0) {\
+	if (parent_node != &_rb_null) {\
 		if (direction) {\
 			parent_node->right = current_node;\
 		} else {\
 			parent_node->left = current_node;\
 		}\
 	} else {\
-		*tree = current_node;\
+		root = current_node;\
 	}\
 	current_node->parent = parent_node;\
 	current_node->value_ptr = new_value_ptr;\
 	current_node->left = &_rb_null;\
 	current_node->right = &_rb_null;\
-	current_node->color = 0;\
+	current_node->color = _COLOR_RED;\
+	_LLIB_RB_INSERT_FIXUP(root, current_node);\
+	root->color = _COLOR_BLACK;\
 }
 
-#define LLIB_RB_TRAVERSE_INORDER(tree, variable_name, action) {\
-	int stack_size = 0;\
-	llib_rb_node_t *current_node = tree;\
-	while(stack_size > 0 || current_node != &_rb_null) {\
-		if(current_node != &_rb_null) {\
-			_node_arr[stack_size++] = current_node;\
-			current_node = current_node->left;\
+#define _LLIB_RB_INSERT_FIXUP(root, node_ptr) {\
+	while(node_ptr->parent->color != _COLOR_BLACK) {\
+		if(_LLIB_RB_IS_RIGHT_CHILD(node_ptr->parent)) {\
+			if (node_ptr->parent->parent->left->color == _COLOR_RED) {\
+				node_ptr->parent->parent->left->color = _COLOR_BLACK;\
+				node_ptr->parent->parent->color = _COLOR_RED;\
+				node_ptr->parent->color = _COLOR_BLACK;\
+				node_ptr = node_ptr->parent->parent;\
+			} else {\
+				if(!_LLIB_RB_IS_RIGHT_CHILD(node_ptr)) {\
+					_LLIB_RB_ROTATE_RIGHT(root, node_ptr->parent);\
+					node_ptr = node_ptr->right;\
+				}\
+				_LLIB_RB_ROTATE_LEFT(root, node_ptr->parent->parent);\
+				node_ptr->parent->color = _COLOR_BLACK;\
+				node_ptr->parent->left->color = _COLOR_RED;\
+			}\
 		} else {\
-			current_node = _node_arr[--stack_size];\
-			void * variable_name = current_node->value_ptr;\
-			action;\
-			current_node = current_node->right;\
+			if (node_ptr->parent->parent->right->color == _COLOR_RED) {\
+				node_ptr->parent->parent->right->color = _COLOR_BLACK;\
+				node_ptr->parent->parent->color = _COLOR_RED;\
+				node_ptr->parent->color = _COLOR_BLACK;\
+				node_ptr = node_ptr->parent->parent;\
+			} else {\
+				if(_LLIB_RB_IS_RIGHT_CHILD(node_ptr)) {\
+					_LLIB_RB_ROTATE_LEFT(root, node_ptr->parent);\
+					node_ptr = node_ptr->left;\
+				}\
+				_LLIB_RB_ROTATE_RIGHT(root, node_ptr->parent->parent);\
+				node_ptr->parent->color = _COLOR_BLACK;\
+				node_ptr->parent->right->color = _COLOR_RED;\
+			}\
 		}\
 	}\
 }
 
-#define _LLIB_RB_MOST_NODE(tree, result_node_ptr, direction) {\
-    llib_rb_node_t *current_node = tree;\
+#define _LLIB_RB_TRAVERSE_INORDER(root, action) {\
+	int _stack_size = 0;\
+	llib_rb_node_t *_current_node = root;\
+	while(_stack_size > 0 || _current_node != &_rb_null) {\
+		if(_current_node != &_rb_null) {\
+			_node_arr[_stack_size++] = _current_node;\
+			_current_node = _current_node->left;\
+		} else {\
+			_current_node = _node_arr[--_stack_size];\
+			action;\
+			_current_node = _current_node->right;\
+		}\
+	}\
+}
+
+#define LLIB_RB_TRAVERSE_INORDER(root, variable_name, action) {\
+	_LLIB_RB_TRAVERSE_INORDER(root, {\
+		void *variable_name = _current_node->value_ptr;\
+		action;\
+	});\
+}
+
+#define _LLIB_RB_MOST_NODE(root, result_node_ptr, direction) {\
+    llib_rb_node_t *current_node = root;\
 	llib_rb_node_t *previous_node = 0;\
     while(current_node != &_rb_null){\
 		previous_node = current_node;\
@@ -76,24 +172,22 @@ llib_rb_node_t *_node_arr[64] = {0};
     *result_node_ptr = previous_node;\
 }
 
-#define _LLIB_RB_MINIMUM_NODE(tree, minimum_node) {\
-	_LLIB_RB_MOST_NODE(tree, minimum_node, left);\
+#define _LLIB_RB_MINIMUM_NODE(root, minimum_node) {\
+	_LLIB_RB_MOST_NODE(root, minimum_node, left);\
 }
 
-#define _LLIB_RB_MAXIMUX_NODE(tree, maximum_node) {\
-	_LLIB_RB_MOST_NODE(tree, minimum_node, right);\
+#define _LLIB_RB_MAXIMUX_NODE(root, maximum_node) {\
+	_LLIB_RB_MOST_NODE(root, minimum_node, right);\
 }
 
-#define _LLIB_RB_IS_RIGHT(node) (node->parent->right == node)
-
-#define _LLIB_RB_SUCCESSOR_NODE(tree, result_node_ptr) {\
-	if (tree == 0) {\
+#define _LLIB_RB_SUCCESSOR_NODE(root, result_node_ptr) {\
+	if (root == 0) {\
 		*result_node_ptr = 0;\
-	} else if (tree->right != &_rb_null) {\
-		_LLIB_RB_MINIMUM_NODE(tree->right, result_node_ptr);\
-	} else if (tree->parent != 0) {\
-		*result_node_ptr = tree;\
-		while(*result_node_ptr->parent != 0 && _LLIB_RB_IS_RIGHT(*result_node_ptr)) {\
+	} else if (root->right != &_rb_null) {\
+		_LLIB_RB_MINIMUM_NODE(root->right, result_node_ptr);\
+	} else if (root->parent != 0) {\
+		*result_node_ptr = root;\
+		while(*result_node_ptr->parent != 0 && _LLIB_RB_IS_RIGHT_CHILD(*result_node_ptr)) {\
 			*result_node_ptr = *result_node_ptr->parent;\
 		}\
 		*result_node_ptr = *result_node_ptr->parent;\
@@ -102,10 +196,12 @@ llib_rb_node_t *_node_arr[64] = {0};
 	}\
 }
 
-#define _LLIB_RB_HEIGHT(tree, height_ptr) {\
-	int head = 0, tail = 0, height = 0;\
+#define _LLIB_RB_HEIGHT(root, height_ptr) {\
+	*height_ptr = -1;\
+	_LLIB_RB_TRAVERSE_INORDER(root, {\
+		if (_stack_size > *height_ptr) *height_ptr = _stack_size;\
+	});\
+	*height_ptr += 1;\
 }
 
 #endif /* LLIB_H */
-
-
--- test.c
+++ test.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "unity/unity.h"
 #include "llib.h"
-
-
 
 #define COMPARE_INT(a, b) ((*(int*)a) - (*(int*)b))
 #define VALUE(node) (node ? *(int*)(node->value_ptr) : 0)
 
+int compare_int(const void *a, const void *b) {
+	return *(int*)a - *(int*)b;
+}
+
 void test_LLIB_RB() {
 	llib_rb_tree_t * tree;
-	LLIB_INIT_RB_TREE(&tree);
+	LLIB_INIT_RB_TREE(tree);
 
-	int values[] = {2,5,3,8,4,20,7,9,12};
+	int values[] = {30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
 	int length = sizeof(values) / sizeof(values[0]);
 	for(int i = 0; i < length; i++) {
-		LLIB_RB_INSERT(&tree, &values[i], COMPARE_INT);
+		LLIB_RB_INSERT(tree, &values[i], COMPARE_INT);
 	}
 
-	int sorted_values[] = {2,3,4,5,7,8,9,12,20};
+	int *sorted_values = malloc(sizeof(values) * sizeof(int));
+	memcpy(sorted_values, values, sizeof(values) * sizeof(int));
+	qsort(sorted_values, length, sizeof(int), compare_int);
 	int result_values[length], i = 0;
 	LLIB_RB_TRAVERSE_INORDER(tree, value_ptr, result_values[i++] = *(int*)value_ptr);
 	TEST_ASSERT_EQUAL_INT_ARRAY(sorted_values, result_values, length);
 
 	llib_rb_node_t *minimum;
 	_LLIB_RB_MINIMUM_NODE(tree, &minimum);
-	TEST_ASSERT_EQUAL_INT(2, VALUE(minimum));
+	TEST_ASSERT_EQUAL_INT(sorted_values[0], VALUE(minimum));
 
 	llib_rb_node_t *current = minimum;
 	for(int i = 0; i < length; i++) {
@@ -34,10 +40,16 @@ void test_LLIB_RB() {
 		current = successor;
 	}
 	TEST_ASSERT_EQUAL_INT_ARRAY(result_values, sorted_values, length);
+
+	int height;
+	_LLIB_RB_HEIGHT(tree, &height);
+	printf("Height of the tree is %d\n", height);
 }
 
 int main() {
 	UNITY_BEGIN();
 	RUN_TEST(test_LLIB_RB);
+
+	test_LLIB_RB();
 	return UNITY_END();
 }
